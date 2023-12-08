00596167e4782a687b827c41fd916a8774bea3fb
Tamaaraa <44346875+Tamaaraa@users.noreply.github.com>
2023-12-08 11:18:53 +0100

    Day 8 part 1

--- day_08/Program.cs
+++ day_08/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AdventOfCode;
+
+static class Program
+{
+    static void Main()
+    {
+        string input = File.ReadAllText("./input");
+        Dictionary<string, Dictionary<char, string>> pathDict = new();
+
+        string directions = input.Split("\n\n")[0];
+        foreach (string line in input.Split("\n\n")[1].Split("\n"))
+        {
+            string root = line.Split(" = ")[0];
+            string left = line.Split(" = ")[1].Split(" ")[0][1..4];
+            string right = line.Split(" = ")[1].Split(" ")[1][..3];
+
+            pathDict[root] = new() { ['L'] = left, ['R'] = right };
+        }
+
+        int steps1 = 0;
+        string pos = "AAA";
+        while (pos != "ZZZ")
+        {
+            foreach (char ch in directions)
+            {
+                pos = pathDict[pos][ch];
+                steps1++;
+                if (pos == "ZZZ")
+                {
+                    break;
+                }
+            }
+        }
+        Console.WriteLine(steps1);
+    }
+}
